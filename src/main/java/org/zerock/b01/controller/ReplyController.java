17f9faaf75c4b0f0425f7b2f20bdd49bbb1b0feb
kyh <kyh@tigensoft.co.kr>
2024-06-17 16:42:30 +0900

    자바 웹 개발 워크북 6장 //6.1 'Rest 방식의 서비스' (pg.528) 까지

---
+++ src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -0,0 +1,39 @@
+package org.zerock.b01.controller;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zerock.b01.dto.ReplyDTO;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/replies")
+public class ReplyController {
+
+    @ApiOperation(value = "Replies POST", notes = "POST 방식으로 댓글 등록")
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Long> register(@Valid @RequestBody ReplyDTO replyDTO,
+                                                      BindingResult bindingResult) throws BindException {
+        log.info(String.valueOf(replyDTO));
+
+        if (bindingResult.hasErrors()) {
+            throw new BindException(bindingResult);
+        }
+
+        HashMap<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno", 111L);
+
+        return resultMap;
+    }
+}
