e1a0162b9ac86c773dc8c78e633788a06ce40a8a
kyh <kyh@tigensoft.co.kr>
2024-06-21 17:03:20 +0900

    자바 웹 개발 워크북 7장 - '파일 업로드 처리' (완)

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -1,7 +1,10 @@
 package org.zerock.b01.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,19 +12,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.zerock.b01.dto.BoardDTO;
-import org.zerock.b01.dto.BoardListReplyCountDTO;
-import org.zerock.b01.dto.PageRequestDTO;
-import org.zerock.b01.dto.PageResponseDTO;
+import org.zerock.b01.dto.*;
 import org.zerock.b01.service.BoardService;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@Slf4j
+@Log4j2
 @RequestMapping("/board")
 public class BoardController {
+
+    @Value("${org.zerock.upload.path}")
+    private String uploadPath;
 
     private final BoardService boardService;
 
@@ -30,9 +36,9 @@ public class BoardController {
 
 //        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
 
-        PageResponseDTO<BoardListReplyCountDTO> responseDTO = boardService.listWithReplyCount(pageRequestDTO);
+        PageResponseDTO<BoardListAllDTO> responseDTO = boardService.listWithAll(pageRequestDTO);
 
-        log.info(String.valueOf(responseDTO));
+        log.info(responseDTO);
 
         model.addAttribute("responseDTO", responseDTO);
 
@@ -62,7 +68,7 @@ public class BoardController {
     @GetMapping({"/read", "/modify"})
     public void readGet(Long bno, PageRequestDTO pageRequestDTO, Model model) {
         BoardDTO boardDTO = boardService.readOne(bno);
-        log.info(String.valueOf(boardDTO));
+        log.info(boardDTO);
         model.addAttribute("dto", boardDTO);
     }
 
@@ -89,10 +95,41 @@ public class BoardController {
     }
 
     @PostMapping("/remove")
-    public String remove(Long bno, RedirectAttributes redirectAttributes) {
-        log.info("remove POST..........");
+    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
+
+        Long bno = boardDTO.getBno();
+        log.info("remove POST..........{}", bno);
+
         boardService.remove(bno);
+
+        //게시물이 데이터베이스 상에서 삭제되었다면 첨부파일 삭제
+        log.info(boardDTO.getFileNames());
+        List<String> fileNames = boardDTO.getFileNames();
+        if (fileNames != null && fileNames.size() > 0) {
+            removeFiles(fileNames);
+        }
         redirectAttributes.addFlashAttribute("result", "removed");
         return "redirect:/board/list";
+    }
+
+    public void removeFiles(List<String> files) {
+        for (String fileName : files) {
+            FileSystemResource resource = new FileSystemResource(uploadPath + File.separator + fileName);
+            String resourceName = resource.getFilename();
+
+            try {
+
+                String contentType = Files.probeContentType(resource.getFile().toPath());
+                resource.getFile().delete();
+
+                //섬네일이 존재한다면
+                if (contentType.startsWith("image")) {
+                    File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+                    thumbnailFile.delete();
+                }
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        }
     }
 }
