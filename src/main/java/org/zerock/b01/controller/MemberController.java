eb364ec143901ab34b4d706545635d6e275246ab
kyh <kyh@tigensoft.co.kr>
2024-06-26 15:12:51 +0900

    자바 웹 개발 워크북 8장 - '스프링 시큐리티'  //8.2 '회원 데이터 처리' (pg.738) 까지

--- src/main/java/org/zerock/b01/controller/MemberController.java
+++ src/main/java/org/zerock/b01/controller/MemberController.java
@@ -4,13 +4,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.b01.dto.MemberJoinDTO;
+import org.zerock.b01.service.MemberService;
 
 @Log4j2
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping("/login")
     public void loginGET(String error, String logout) {
@@ -20,5 +26,27 @@ public class MemberController {
         if (logout != null) {
             log.info("user logout........");
         }
+    }
+
+    @GetMapping("/join")
+    public void joinGET() {
+        log.info("join get...");
+    }
+
+    @PostMapping("/join")
+    public String joinPOST(MemberJoinDTO memberJoinDTO, RedirectAttributes redirectAttributes) {
+        log.info("join post...");
+        log.info(memberJoinDTO);
+
+        try {
+            memberService.join(memberJoinDTO);
+        } catch (MemberService.MidExistException e) {
+            redirectAttributes.addFlashAttribute("error", "mid");
+            return "redirect:/member/join";
+        }
+
+        redirectAttributes.addFlashAttribute("result", "success");
+
+        return "redirect:/member/login";
     }
 }
--- src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
+++ src/main/java/org/zerock/b01/security/CustomUserDetailsService.java
@@ -2,31 +2,51 @@ package org.zerock.b01.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.zerock.b01.domain.Member;
+import org.zerock.b01.dto.MemberSecurityDTO;
+import org.zerock.b01.repository.MemberRepository;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final PasswordEncoder passwordEncoder;
-
-    public CustomUserDetailsService() {
-        this.passwordEncoder = new BCryptPasswordEncoder();
-    }
+    private final MemberRepository memberRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         log.info("loadUserByUsername: {}", username);
 
-        UserDetails userDetails = User.builder().username("user1").password(passwordEncoder.encode("1111")).authorities("ROLE_USER").build();
+        Optional<Member> result = memberRepository.getWithRoles(username);
 
-        return userDetails;
+        if (result.isEmpty()) {
+            throw new UsernameNotFoundException("username not found...");
+        }
+
+        Member member = result.get();
+
+        MemberSecurityDTO memberSecurityDTO = new MemberSecurityDTO(
+                member.getMid(),
+                member.getMpw(),
+                member.getEmail(),
+                member.isDel(),
+                false,
+                member.getRoleSet().stream().map(memberRole -> new SimpleGrantedAuthority("ROLE_" + memberRole.name()))
+                        .collect(Collectors.toList())
+        );
+
+        log.info("memberSecurityDTO");
+        log.info(memberSecurityDTO);
+
+        return memberSecurityDTO;
     }
 }
